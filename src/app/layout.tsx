bdb3200cdd5ee36529abbd9f49d59d66d6e3f7d7
arunsunderraj <arunsunderraj17@gmail.com>
2026-01-06 13:40:44 +0530

    Re-enable ChatBot component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
           <SessionProvider>
             <Analytics />
             {children}
-            {/* <ChatBot /> */}{/* Hidden for now */}
+            <ChatBot />
           </SessionProvider>
         </ThemeProvider>
       </body>
