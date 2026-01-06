9ebcb778c9e9c83b338f0d70f24a7b8934eae0cd
arunsunderraj <arunsunderraj17@gmail.com>
2026-01-06 13:35:08 +0530

    Hide ChatBot component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
           <SessionProvider>
             <Analytics />
             {children}
-            <ChatBot />
+            {/* <ChatBot /> */}{/* Hidden for now */}
           </SessionProvider>
         </ThemeProvider>
       </body>
